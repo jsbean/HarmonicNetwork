da1856e2299d45f6ac8acdbe6a859a23555225d7
James Bean <jbean46@gmail.com>
2019-08-14 10:38:40 +0200

    Add redo functionality

--- Sources/HarmonicPathfinder/main.swift
+++ Sources/HarmonicPathfinder/main.swift
@@ -5,26 +5,33 @@ let console: Console = Terminal()
 var input = CommandInput(arguments: CommandLine.arguments)
 
 var path: Stack<String> = []
+var redo: Stack<String> = []
 
 var chord = "I"
 path.push(chord)
 console.print("Let's start on the Tonic: \(path)")
 while true {
-    let options = Array(bachMajor.neighbors(of: chord)) + ["undo"]
+    let options = Array(bachMajor.neighbors(of: chord)) + ["undo", "redo"]
     let selection = console.choose("What's next?",
         from: options, display: { option in
-            if option == "undo" { return option.consoleText() }
-            return ConsoleText(
-                stringLiteral: "\(option): \(bachMajor.weight(from: chord, to: option)!)"
-            )
+            switch option {
+            case "undo", "redo":
+                return option.consoleText()
+            default:
+                return ConsoleText(
+                    stringLiteral: "\(option): \(bachMajor.weight(from: chord, to: option)!)"
+                )
+            }
         }
     )
-    if selection == "undo" {
-        _ = path.pop()
-        console.print("Harmonic Path: \(path)")
-    } else {
+    switch selection {
+    case "undo":
+        path.pop().map { redo.push($0) }
+    case "redo":
+        redo.pop().map { path.push($0) }
+    default:
         chord = selection
         path.push(chord)
-        console.print("Harmonic Path: \(path)")
     }
+    console.print("Harmonic Path: \(path)")
 }
