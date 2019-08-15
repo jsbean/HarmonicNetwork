ced524d236e5a64f6ef60ba99e9dc3e931b1d60a
James Bean <jbean46@gmail.com>
2019-08-15 17:19:15 +0200

    Ask for 0.16.0 from dn-m/Music

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "HarmonicNetwork",
     dependencies: [
         .package(url: "https://github.com/dn-m/Structure", from: "0.24.0"),
-        .package(url: "https://github.com/dn-m/Music", from: "0.15.0"),
+        .package(url: "https://github.com/dn-m/Music", from: "0.16.0"),
         .package(url: "https://github.com/dn-m/NotationModel", from: "0.8.0"),
         .package(url: "https://github.com/dn-m/Math", from: "0.8.0"),
         .package(url: "https://github.com/vapor/console-kit", .upToNextMinor(from: "3.1.1"))
