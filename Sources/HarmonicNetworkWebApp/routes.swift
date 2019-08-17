07ff12c02b525024a77e7c7c6abe6cbd8cc4fea3
James Bean <jbean46@gmail.com>
2019-08-17 16:23:36 +0200

    Tidy up home page

--- Sources/HarmonicNetworkWebApp/routes.swift
+++ Sources/HarmonicNetworkWebApp/routes.swift
@@ -6,29 +6,6 @@ import Vapor
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
 
-    router.get("hello") { req -> String in "Welcome to the Harmonic Network web app!" }
-
-//    router.get { req -> Future<View> in
-//        return Chord.query(on: req).all().flatMap(to: View.self) { chords in
-//            let nodes = chords.map { chord in
-//                NodeView(
-//                    id: chord.id,
-//                    label: chord.label,
-//                    function: chord.function,
-//                    x: Double.random(in: 100..<700),
-//                    y: Double.random(in: 100..<700)
-//                )
-//            }
-//            return try req.view().render("home", ["nodes": nodes])
-//        }
-//    }
-//
-//    router.post(Chord.self, at: "add") { req, chord -> Future<Response> in
-//        return chord.save(on: req).map(to: Response.self) { chord in
-//            dump(chord)
-//
-//            return req.redirect(to: "/")
-//        }
-//    }
+    // Home page
+    router.get { req -> String in "Welcome to the Harmonic Network web app!" }
 }
-
