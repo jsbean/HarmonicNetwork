3f0254dfe2717c66c8e7dc9a3e791815ce571ac8
James Bean <jbean46@gmail.com>
2019-08-17 14:40:15 +0200

    Migrate over package details

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         .package(url: "https://github.com/dn-m/NotationModel", from: "0.9.0"),
         .package(url: "https://github.com/dn-m/Math", from: "0.8.0"),
         .package(url: "https://github.com/dn-m/Graphics", from: "0.2.0"),
-        .package(url: "https://github.com/vapor/console-kit", .upToNextMinor(from: "3.1.1"))
+        .package(url: "https://github.com/vapor/console-kit", .upToNextMinor(from: "3.1.1")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0")),
     ],
     targets: [
         .target(
@@ -17,7 +18,15 @@ let package = Package(
             dependencies: ["DataStructures", "Pitch", "Math", "SpelledPitch", "Geometry"]
         ),
         .target(
-            name: "harmonic-network",
+            name: "harmonic-network-webapp",
+            dependencies: ["Vapor"]
+        ),
+        .target(
+            name: "harmonic-network-server",
+            dependencies: ["harmonic-network-webapp"]
+        ),
+        .target(
+            name: "harmonic-network-cli",
             dependencies: ["DataStructures", "Pitch", "Math", "SpelledPitch", "Console", "Logging", "Command"]
         ),
         .testTarget(
