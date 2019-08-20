68ba2ec6203b115db653da29c1a7101e1d66a586
James Bean <jbean46@gmail.com>
2019-08-20 12:15:44 +0200

    Create node for current chord and neighbors

--- Public/scripts/find-path.js
+++ Public/scripts/find-path.js
@@ -9,30 +9,30 @@ function findPath() {
 
 function continuePath(path, redo) {
 
-  prepareDoneStartOverButton(() => {
-    updatePathLabel("All done: " + path);
-    disableUndoButton();
-    disableRedoButton();
-    clearNeighborButtons();
-    toggleDoneButtonToStartOver(() => {
-      findPath();
-    });
-  });
+  // prepareDoneStartOverButton(() => {
+  //   updatePathLabel("All done: " + path);
+  //   disableUndoButton();
+  //   disableRedoButton();
+  //   clearNeighborButtons();
+  //   toggleDoneButtonToStartOver(() => {
+  //     findPath();
+  //   });
+  // });
 
-  prepareUndoButton(() => {
-    // TODO: Refactor into an API call "undo"
-    redo.push(path.pop());
-    continuePath(path,redo);
-  });
+  // prepareUndoButton(() => {
+  //   // TODO: Refactor into an API call "undo"
+  //   redo.push(path.pop());
+  //   continuePath(path,redo);
+  // });
 
-  prepareRedoButton(() => {
-    // TODO: Refactor into an API call "redo"
-    path.push(redo.pop());
-    continuePath(path,redo);
-  });
+  // prepareRedoButton(() => {
+  //   // TODO: Refactor into an API call "redo"
+  //   path.push(redo.pop());
+  //   continuePath(path,redo);
+  // });
 
   // Update UI
-  updateUndoRedo(path,redo);
+  // updateUndoRedo(path,redo);
   updatePathLabel(path);
 
   // The current chord
@@ -45,24 +45,65 @@ function continuePath(path, redo) {
   // Refactor body into own method
   let neighborsNode = document.getElementById("neighbors");
 
+  const container = document.getElementById("container");
+  const svgContainer = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svgContainer.setAttribute("width", 400);
+  svgContainer.setAttribute("height", 400);
+
+  const currentNode = makeNode("I", 200, 200, 50, "gray");
+
+  // attach it to the container
+  svgContainer.appendChild(currentNode);
+  
+
   // Make a POST request with the current chord label to the address: "neighbors"
   post({ "label": current }, "neighbors", response => {
-    clearNeighborButtons()
 
     let neighbors = JSON.parse(response);
     // Create buttons for each neighbor node
     for (var i = neighbors.length - 1; i >= 0; i--) {
       let neighbor = neighbors[i];
-      let button = document.createElement("button");
-      // TODO: Reintegrate weights
-      button.innerHTML = neighbor.label + ": " + neighbor.probability;
-      button.name = neighbor;
-      button.className = "neighbor";
-      button.onclick = () => proceedWithChord(neighbor.label, path);
-      neighborsNode.insertBefore(button, neighborsNode.childNodes[0]);
+      const neighborNode = makeNode(neighbor.label, Math.random() * 300 + 50, Math.random() * 300 + 50, 50, "lightgray");
+      svgContainer.appendChild(neighborNode);
+
+      
+      // let button = document.createElement("button");
+      // // TODO: Reintegrate weights
+      // button.innerHTML = neighbor.label + ": " + neighbor.probability;
+      // button.name = neighbor;
+      // button.className = "neighbor";
+      // button.onclick = () => proceedWithChord(neighbor.label, path);
+      // neighborsNode.insertBefore(button, neighborsNode.childNodes[0]);
     }
   });
+
+  container.appendChild(svgContainer);
 };
+
+function makeNode(text, x, y, width, color, callback) {
+  // Create group container
+  const container = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  // Create circle
+  const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  circle.setAttribute("cx", x);
+  circle.setAttribute("cy", y);
+  circle.setAttribute("r", 0.5 * width);
+  circle.setAttribute("fill", color);
+  circle.setAttribute("stroke", "gray");
+  // Create text label
+  const label = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  label.textContent = text;
+  label.setAttribute("font-size", width / 3);
+  label.setAttribute("x", x);
+  label.setAttribute("y", y);
+  label.setAttribute("fill", "white");
+  label.setAttribute("text-anchor", "middle");
+  label.setAttribute("dy", ".3em");
+  // Compose SVG
+  container.appendChild(circle);
+  container.appendChild(label);
+  return container
+}
 
 function proceedWithChord(chord, path) {
   path.push(chord);
