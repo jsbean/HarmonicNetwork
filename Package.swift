7b9999366b1abb6324e8b0136490f45ada5c854a
James Bean <jbean46@gmail.com>
2019-08-21 20:19:26 +0200

    Point to new Graphics package

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .package(url: "https://github.com/dn-m/Music", from: "0.16.0"),
         .package(url: "https://github.com/dn-m/NotationModel", from: "0.9.0"),
         .package(url: "https://github.com/dn-m/Math", from: "0.8.0"),
-        .package(url: "https://github.com/dn-m/Graphics", from: "0.3.0"),
+        .package(url: "https://github.com/dn-m/Graphics", from: "0.4.0"),
         .package(url: "https://github.com/vapor/vapor.git", .exact("3.3.0")),
         .package(url: "https://github.com/vapor/leaf.git", .upToNextMinor(from: "3.0.0")),
     ],
